1c95c13b7c7cd00645d33659a7a5be912d97b704
Lee Nuckols <lee.nuckols@gmail.com>
2025-11-01 09:48:33 -0500

    Updated for queen

---
+++ bin/list.sh
@@ -0,0 +1,5 @@
+find /usr/share/applications ~/.local/share/applications -iname '*.desktop' -print0 | while IFS= read -r -d $'\0' d; do
+  for m in $(grep MimeType "$d" | cut -d= -f2 | tr ";" " "); do
+    echo "'$d'" "'$m'"
+  done
+done
